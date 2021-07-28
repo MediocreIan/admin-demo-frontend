fc5f08db0c47d75d37b435f8e4d8b603b75edcb0
Ian Drews <iandrews@Ians-MacBook-Pro.local>
2021-07-28 17:17:54 -0500

    price changets no Doi

--- src/Detail.js
+++ src/Detail.js
@@ -2,16 +2,12 @@ import React, { useState, useEffect } from 'react'
 import Form from './components/Form'
 import data from './components/Form/data'
 import { useParams } from 'react-router-dom'
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-
+import Price from './Price'
 
 export default function Landing(props) {
     const [product, setProduct] = useState({})
     const [attributes, setAttributes] = useState(null)
-    const [price, setPrice] = useState(0)
-    const [currencies, setCurrencies] = useState(0)
-    const [activeCurrency, setCurrency] = useState(0)
+    const [playerLoaded, setPlayerLoaded] = useState(false)
 
     let { userId, productId } = useParams()
 
@@ -29,7 +25,6 @@ export default function Landing(props) {
                 .then(response => response.json())
                 .then(result => {
                     setProduct(result)
-                    console.log("########################################", result)
                 })
                 .catch(error => console.log('error', error))
         }
@@ -48,39 +43,14 @@ export default function Landing(props) {
                     window.player = api
                     window.configurator = await api.getConfigurator()
                     await api.when('loaded')
+                    setPlayerLoaded(true)
                     if (attributes === null) {
                         setAttributes(window.configurator.getDisplayAttributes())
+                        const privateConfig = window.player.enableApi('player').getConfigurator()
+                        privateConfig.on('setConfiguration', config => {
+                            setAttributes(window.configurator.getDisplayAttributes())
+                        });
                     }
-
-                    var myHeaders = new Headers();
-                    myHeaders.append("Authorization", "Bearer 34613bc7-dd6d-41f3-b1e0-6098b29b5d1a");
-
-                    var requestOptions = {
-                        method: 'GET',
-                        headers: myHeaders,
-                        redirect: 'follow'
-                    };
-
-                    fetch("https://admin.demo.threekit.com/pricebook/" + userId, requestOptions)
-                        .then(response => response.json())
-                        .then(result => {
-                            let pricebook = result.pricebooks.filter((book) => {
-                                return book.name.toLowerCase() === 'website'
-                            })
-
-                            if (pricebook) {
-                                pricebook = pricebook[0]
-                                let currentPrice = window.configurator.getPrice(pricebook.id, pricebook.currencies[0])
-                                if (currentPrice !== price) {
-                                    setPrice(currentPrice)
-                                }
-
-                                console.log(price)
-                            }
-                        })
-                        .catch(error => console.log('error', error));
-
-
                 })
         }
         return () => {
@@ -89,32 +59,13 @@ export default function Landing(props) {
             }
         }
     }, [product, attributes, props.match.params.userId])
-    console.log(product)
     return (
         <>
             <div id='player' style={{
                 height: "45vh"
             }}></div>
             {attributes === null || attributes.length === 0 ? null : <Form data={attributes} />}
-            <Box style={{
-                margin: "auto",
-                width: "100%",
-                display: 'flex',
-                alignItems: "center",
-                justifyContent: "center",
-                position: 'fixed',
-                bottom: "5px"
-            }}>
-                <Paper elevation={6} style={{
-                    borderRadius: "10px",
-                    backgroundColor: "#3f51b5"
-                }}>
-                    <h2 style={{
-                        margin: "10px",
-                        color: "white"
-                    }}>Price: ${price}</h2>
-                </Paper>
-            </Box>
+            <Price userId={userId} key={playerLoaded} />
         </>
     )
 }
