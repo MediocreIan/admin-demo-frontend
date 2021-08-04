5f546c114a5e0b7f0407122d5d04c31388910e73
Ian Drews <iandrews@Ians-MacBook-Pro.local>
2021-08-04 09:39:52 -0500

    fixed pricing

--- src/Price.js
+++ src/Price.js
@@ -6,56 +6,70 @@ export default function Price(props) {
     const [price, setPrice] = useState(0)
     const [currencies, setCurrencies] = useState(0)
     const [activeCurrency, setCurrency] = useState(0)
+    const [attributes, setAttributes] = useState(props.attributes);
+    const [pricebook, setPricebook] = useState(null);
 
     useEffect(() => {
+        console.log("RELOAD")
+        // setAttributes(props.attributes)
         let userId = props.userId
 
-        // var myHeaders = new Headers();
-        // myHeaders.append("Authorization", "Bearer 34613bc7-dd6d-41f3-b1e0-6098b29b5d1a");
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer 34613bc7-dd6d-41f3-b1e0-6098b29b5d1a");
 
         var requestOptions = {
             method: 'GET',
-            // headers: myHeaders,
+            headers: myHeaders,
             redirect: 'follow'
         };
 
-        fetch("https://admin.demo.threekit.com/pricebook/" + userId, requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                let pricebook = result.pricebooks.filter((book) => {
-                    return book.name.toLowerCase() === 'website'
-                })
 
-                if (pricebook) {
-                    pricebook = pricebook[0]
-                    if (window.configurator) {
-                        let currentPrice = window.configurator.getPrice(pricebook.id, pricebook.currencies[0])
-                        if (currentPrice !== price) {
-                            setPrice(currentPrice)
+        if (!pricebook) {
+            fetch("https://admin.demo.threekit.com/pricebook/" + userId, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    let ApiPricebook = result.pricebooks.filter((book) => {
+                        return book.name.toLowerCase() === 'website'
+                    })
+
+                    if (ApiPricebook) {
+                        setPricebook(ApiPricebook)
+                        ApiPricebook = ApiPricebook[0]
+                        if (window.configurator) {
+                            let currentPrice = window.configurator.getPrice(ApiPricebook.id, ApiPricebook.currencies[0])
+                            if (currentPrice !== price) {
+                                setPrice(currentPrice)
+                            }
                         }
-                        const privateConfig = window.player.enableApi('player').getConfigurator()
-                        privateConfig.on('setConfiguration', config => setPrice(window.configurator.getPrice(pricebook.id, pricebook.currencies[0])));
                     }
+                })
+                .catch(error => console.log('error', error));
+        } else {
+            if (window.configurator) {
+                console.log(pricebook)
+                let currentPrice = window.configurator.getPrice(pricebook[0].id, pricebook[0].currencies[0])
+                if (currentPrice !== price) {
+                    setPrice(currentPrice)
                 }
-            })
-            .catch(error => console.log('error', error));
+            }
+        }
 
 
-    }, [price])
+    }, [price, props])
 
     return (
-        <>{price > 0 ? (
+        <div key={props.key}>{price > 0 ? (
             <Box style={props.styles}>
-                <Paper elevation={2} style={{
-                    borderRadius: "5px",
-                    backgroundColor: "#F4F4F4"
+                <Paper elevation={6} style={{
+                    borderRadius: "10px",
+                    border: "1px solid #79AC85"
                 }}>
                     <h2 style={{
-                        margin: "5px",
+                        margin: "2px",
                         color: "#0F2526"
                     }}>Price: ${price}</h2>
                 </Paper>
             </Box>
-        ) : null}        </>
+        ) : null}        </div>
     )
 }
